961cdb64bacbdb37332602fb3e59ccdf1f203ada
oacarrillo <ocarrillo@gmail.com>
2022-11-16 09:03:29 -0500

    Clase 5 ejercicio

---
+++ Sesion 5/UnitTestingClase5 - Clase Suma/UnitTestingClase5/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingClase5
+{
+    public class Calc
+    {
+        public static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
